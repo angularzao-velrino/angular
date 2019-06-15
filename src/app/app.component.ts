afe1648d7207eba8ebb766f3a375f8bd209eb3f4
velrino <denismagalhaesvelrino@gmail.com>
2019-06-15 12:41:00 -0300

    mat-table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Example {
+  name: string;
+  position: number;
+  value: string;
+}
+
+const ELEMENT_DATA: Example[] = [
+  { position: 1, name: 'Hydrogen', value: 'H' },
+  { position: 2, name: 'Helium',  value: 'He' },
+  { position: 3, name: 'Lithium', value: 'Li' },
+  { position: 4, name: 'Beryllium', value: 'Be' },
+  { position: 5, name: 'Boron', value: 'B' },
+  { position: 6, name: 'Carbon', value: 'C' },
+  { position: 7, name: 'Nitrogen', value: 'N' },
+  { position: 8, name: 'Oxygen', value: 'O' },
+  { position: 9, name: 'Fluorine', value: 'F' },
+  { position: 10, name: 'Neon', value: 'Ne' },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +26,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loremipsum';
+  displayedColumns: string[] = ['position', 'name', 'value'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
